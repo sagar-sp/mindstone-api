92d7a2a994ac32edea9827995c25da5b36d217e1
sagar23399 <sagarpatil23399@gmail.com>
2021-11-15 16:52:19 +0530

    first commit

---
+++ src/main/java/com/mindstone/mindstoneapi/MindstoneApiApplication.java
@@ -0,0 +1,13 @@
+package com.mindstone.mindstoneapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindstoneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindstoneApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mindstone/mindstoneapi/MindstoneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindstone.mindstoneapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindstoneApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
